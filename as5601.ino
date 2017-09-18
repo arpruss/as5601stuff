0873bb7898d4825ab814e1b2d5bd7a15a4c8a4a1
arpruss <arpruss@gmail.com>
2017-09-17 22:53:20 -0500

    foo

---
+++ as5601.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+
+// 20 SDA, 21 SCL on Mega
+
+const uint8_t deviceAddress = 0x36;
+const uint8_t reg_rawAngleHigh = 0x0C;
+const uint8_t reg_rawAngleLow = 0x0D;
+const uint8_t reg_agc = 0x1A;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+}
+
+String formatAngle(uint16_t angle) {
+// convert 12 bit angle to decimal
+//  Serial.println("angle="+String(angle));
+  uint32_t angle360 = (uint32_t)angle * 360;
+  uint16_t deg = (angle360>>12);
+  uint8_t frac = (100*(angle360&0xFFF))>>12;
+  return String(deg)+"."+String(frac/10)+String(frac%10);
+}
+
+int getByte(uint8_t reg) {
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(reg); // angle high
+  Wire.endTransmission();
+  Wire.requestFrom(deviceAddress,1);
+  if (1<=Wire.available()) {
+    return 0xFF & Wire.read();
+  }
+  else {
+    return -1;
+  }
+}
+
+void loop() {
+  int angleHigh = getByte(reg_rawAngleHigh);
+  if (0 <= angleHigh) {
+    int angleLow = getByte(reg_rawAngleLow);
+    if (0 <= angleLow) {
+      uint16_t angle = (angleHigh<<8) | angleLow;
+      Serial.print(formatAngle(angle));
+    }
+  }
+  int agc = getByte(reg_agc);
+  if (0 <= agc) {
+    Serial.print(" "+String(agc));
+  }
+  Serial.println("");
+}
